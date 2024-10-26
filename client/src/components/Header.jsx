1e1b302915eb4b0fb366148c386ad00774da7e35
Sumit0925 <smtangural@gmail.com>
2024-10-26 19:00:09 +0530

    1st Commit

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import styled from "styled-components";
+import Navbar from "./Navbar";
+
+const Header = () => {
+  const MainHeader = styled.header`
+    padding: 0 4.8rem;
+    height: 10rem;
+    background-color: ${({ theme }) => theme.colors.bg};
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    position: relative;
+
+    .logo {
+      height: 5rem;
+    }
+  `;
+
+  return (
+    <MainHeader>
+      <NavLink to="/">
+        <img className="logo" src="./images/My_Logo.svg" alt="my logo image" />
+
+      </NavLink>
+      <Navbar />
+    </MainHeader>
+  );
+};
+
+export default Header;
